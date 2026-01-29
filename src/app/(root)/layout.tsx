5e58b9c0b04494361062f01cb654deb3234b9ec5
Mehul Prajapati <ailearn90@gmail.com>
2026-01-30 00:47:26 +0530

    fix: require auth when on landing page

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,9 @@
 import RootProvider from '@/components/provider/root-provider'
+import { requireAuth } from '@/utils/auth-utils';
 import React from 'react'
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+    await requireAuth();
     return (
         <RootProvider>
             {children}
